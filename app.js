9386a56f4bba92653eda952f4e12b3daa0055557
pablo <pablo.mandile@gmail.com>
2022-12-10 12:34:20 -0300

    muestro las imagenes correctamente

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ const cargarPeliculas = async() =>{
       datos.results.forEach(pelicula => {
         peliculas += `
           <div class="pelicula">
-            <img class"poster" src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}">
+            <img class"poster" src="https://image.tmdb.org/t/p/w200/${pelicula.poster_path}">
             <h3 class="titulo">${pelicula.title}</h3>
           </div>  
         `;
